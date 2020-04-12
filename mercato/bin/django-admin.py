d4d48e75c0e14d8238a2249d2ec1da551339cee8
Essam <essamabuissa@hotmail.com>
2020-04-12 23:40:10 +0300

    Removed mercato

--- mercato/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/essam/Desktop/Development/Mercato_Project/mercato-DRF/mercato/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
