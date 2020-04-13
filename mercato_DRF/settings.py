f844d31881dd9c69307281aaea11ad857ce131c6
Abdelmajeed <shaheen.abdelmajeed:outlook.com>
2020-04-13 23:30:34 +0300

    login_

--- mercato_DRF/settings.py
+++ mercato_DRF/settings.py
@@ -62,6 +62,7 @@ JWT_AUTH = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
