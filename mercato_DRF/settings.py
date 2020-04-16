3d9eb57942f8be24531e8073e9f9ea52def35121
Abdelmajeed <shaheen.abdelmajeed:outlook.com>
2020-04-16 13:47:07 +0300

    MediaFiles Added

--- mercato_DRF/settings.py
+++ mercato_DRF/settings.py
@@ -141,6 +141,6 @@ CORS_ORIGIN_ALLOW_ALL = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = os.path.join(BASE_DIR,'mediafiles')
 MEDIA_URL = '/media/'
 django_heroku.settings(locals())
